6fee0c08d8780baed000d8d5347edb0893d44e6b
samuelbianch <samuelbianch38@gmail.com>
2023-06-13 18:14:22 -0300

    Corrige ClienteVO

--- exec-mapeamento2/src/main/java/ifmt/cba/vo/ClienteVO.java
+++ exec-mapeamento2/src/main/java/ifmt/cba/vo/ClienteVO.java
@@ -2,18 +2,11 @@ package ifmt.cba.vo;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "cliente")
 public class ClienteVO extends PessoaFisicaVO {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    private int id;
 
     @Column(name = "limitecredito")
     private float limiteCredito;
@@ -29,14 +22,6 @@ public class ClienteVO extends PessoaFisicaVO {
     public ClienteVO(String nome, String cpf, float limiteCredito) {
         super(nome, cpf);
         this.limiteCredito = limiteCredito;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public float getLimiteCredito() {
